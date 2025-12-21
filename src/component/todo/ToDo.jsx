a2ba3a2e4108bc33408930b1ea3eaed8848dd670
Haris <mharis@tibxtech.com>
2025-12-21 14:38:19 +0430

    Implemented the delete all and edit todo functility

--- src/component/todo/ToDo.jsx
+++ src/component/todo/ToDo.jsx
@@ -3,15 +3,43 @@ import { useState } from "react";
 const ToDo = () => {
     const [todos, setTodos] = useState([])
     const [task, setTask] = useState("")
+    const [editIndex, setEditIndex] = useState(null)
 
-    const addToDo = () => {
-        setTodos([...todos, task])
-        setTask("")
+    const addOrEditToDo = () => {
+        if(editIndex === null){
+            setTodos([...todos, task])
+            setTask("")
+        }else{
+            const newArray = todos.map((item , index) => {
+                if(index === editIndex){
+                    return task
+                }else{
+                    return item
+                }
+            })
+            setTodos(newArray)
+            removeEdit()
+        }
     }
 
     const remove = (index) => {
         const newArray = todos.filter((_, i) => i != index);
         setTodos(newArray)
+    }
+
+    const deleteAll = () => {
+        setTodos([])
+    }
+
+    const edit = (index) => {
+        const element = todos[index]
+        setTask(element)
+        setEditIndex(index)
+    }
+
+    const removeEdit = () => {
+        setTask("")
+        setEditIndex(null)
     }
 
     return ( 
@@ -24,16 +52,20 @@ const ToDo = () => {
                     value={task}
                     onChange={(e) => setTask(e.target.value)} 
                 />
-                <input onClick={addToDo} type="button" value="Add" />
+                <input onClick={addOrEditToDo} type="button" value={`${editIndex === null ? 'Add' : 'Edit'}`} />
+                {editIndex != null && (
+                    <input onClick={removeEdit} type="button" value="X" />
+                )}
             </div>
             <hr />
-            <button>Delete All</button>
+            <button onClick={deleteAll}>Delete All</button>
             <div>
                <ul>
                 {todos.length === 0 && "There is no task available"}
                 {todos.map((item, index) => (
                     <li key={index}>
                         <input onClick={() => remove(index)} type="button" value="X" />
+                        <input onClick={() => edit(index)} type="button" value="E" />
                         &nbsp;
                         {item}
                     </li>
